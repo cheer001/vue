16b553cacd9b5a647e0151481db28fc2b2e185fd
gerry <2312306565@qq.com>
2020-07-07 19:39:49 +0700

    feat(*):多个组件示例

---
+++ VueAllDemo/vue-05-组件化/component/Footer.js
@@ -0,0 +1,3 @@
+Vue.component("app-footer", {
+  template: '<div class="footer"><h3>底部组件</h3></div>',
+});
---
+++ VueAllDemo/vue-05-组件化/component/Header.js
@@ -0,0 +1,3 @@
+Vue.component("app-header", {
+  template: '<div class="header"><h3>头部组件</h3></div>',
+});
