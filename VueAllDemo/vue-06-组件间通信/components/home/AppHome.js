32d0590cba8b4ad5f1a51983b3e02e4fe8efac6f
gerry <gerrycheer@gmail.com>
2020-07-08 10:43:57 +0800

    feat(*):slot插槽案例

--- VueAllDemo/vue-06-组件间通信/components/home/AppHome.js
+++ VueAllDemo/vue-06-组件间通信/components/home/AppHome.js
@@ -1,7 +1,9 @@
 (function () {
   const template = `<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                         <!--右边上半区域-->
-                        <h1 class="page-header">Dashboard</h1>
+                        <!--<h1 class="page-header">Dashboard</h1>-->
+                        <!--定义插槽-->
+                        <slot name="dashboard"></slot>
                         <!-- 
                             通过v-on绑定自定义事件实现删除功能：
                             @自定义事件名=事件监听函数
