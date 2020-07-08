fcb4b1298e7d1d4b80a2295cbb3efc25cb1342cf
gerry <gerrycheer@gmail.com>
2020-07-08 10:27:15 +0800

    feat(*):自定义事件 1.自定义事件只用于子组件向父组件发送消息(数据) 2.隔代组件或兄弟组件通信此方式不推荐

--- VueAllDemo/vue-06-组件间通信/components/home/Dashboard.js
+++ VueAllDemo/vue-06-组件间通信/components/home/Dashboard.js
@@ -1,6 +1,10 @@
 (function () {
   const template = `<div class="row placeholders">
-                        <div v-for="(hobby,index) in hobbies" :key="index" class="col-xs-6 col-sm-3 placeholder">
+                        <div 
+                            v-for="(hobby,index) in hobbies" 
+                            :key="index" 
+                            class="col-xs-6 col-sm-3 placeholder"
+                        >
                             <img
                                 src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
                                 width="200"
@@ -9,13 +13,22 @@
                                 alt="Generic placeholder thumbnail"
                             />
                             <h4>{{hobby}}</h4>
-                            <span class="text-muted">Something else</span>
+                            <span class="text-muted">
+                                <a href="#" @click="deleteHobby(index)">删除</a>
+                            </span>
                         </div>
                     </div>`;
 
   window.Dashboard = {
     //声明当前子组件接受父组件的属性
     props: ["hobbies"],
+    methods: {
+      //删除喜好
+      deleteHobby(index) {
+        //触发父组件中 delete_hobby 事件进行删除操作
+        this.$emit("delete_hobby", index);
+      },
+    },
     template,
   };
 })();
