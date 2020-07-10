ad8c9701ef7a0eb1c60ea0ea3612b546ed20896a
gerry <gerrycheer@gmail.com>
2020-07-10 15:47:58 +0800

    feat(*): .vue 单文件组件完整版

--- VueAllDemo/webpack-demo6/src/App.vue
+++ VueAllDemo/webpack-demo6/src/App.vue
@@ -1,16 +1,31 @@
 <template>
+  <!-- 一个vue文件中有且仅有一个template -->
   <div>
     <h1>App 根组件</h1>
+    <h2>{{ msg }}</h2>
+    <Foo />
   </div>
 </template>
 
 <script>
+import Foo from "./components/Foo.vue";
+//导出一个默认的成员对象，它就是当前的组件对象， 可以直接在对象中使用 Vue中的选项，如data、methods等
 export default {
   data() {
-    return {};
+    return {
+      msg: "hello gerry",
+    };
   },
-  components: {},
+  // template,此选项不用写，因为上面template标签代表的就是当前的组件模板
+  components: {
+    Foo,
+  },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped>
+/* scoped 作用是将样式只作用到当前组件中，不然会传递到其他 父子组件中 */
+h1 {
+  color: red;
+}
+</style>
---
+++ VueAllDemo/webpack-demo6/src/components/Foo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="container">
+    <h1>我是Foo子组件</h1>
+    <h2>我是Foo子组件 h2---</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+/* scoped 作用是将样式只作用到当前组件中，不然会传递到其他 父子组件中 */
+h2 {
+  color: blue;
+}
+</style>
