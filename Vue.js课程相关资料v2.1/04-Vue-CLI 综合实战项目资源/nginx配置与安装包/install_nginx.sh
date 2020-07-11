3b7b40547deb3f78cb521e2752f4cb26ac6583b9
gerry <gerrycheer@gmail.com>
2020-07-11 09:07:02 +0800

    解压04-Vue-Cli 综合实战项目

---
+++ Vue.js课程相关资料v2.1/04-Vue-CLI 综合实战项目资源/nginx配置与安装包/install_nginx.sh
@@ -0,0 +1,9 @@
+﻿#!/bin/bash
+cd /opt/soft
+tar -zxvf nginx-1.16.0.tar.gz
+cd nginx-1.16.0/
+./configure \
+--prefix=/usr/local/nginx \
+--with-http_stub_status_module \
+--with-http_ssl_module
+make && make install
