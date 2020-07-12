761edd9b28d6c4e985d7c45d56cd56e9d600d475
gerry <2312306565@qq.com>
2020-07-12 21:29:24 +0700

    refacrot(*)初始化06-组件间通信2

---
+++ VueAllDemo/vue-06-组件间通信2/components/home/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row placeholders">
+    <div
+      v-for="(hobby, index) in hobbies"
+      :key="index"
+      class="col-xs-6 col-sm-3 placeholder"
+    >
+      <img
+        src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+        width="200"
+        height="200"
+        class="img-responsive"
+        alt="Generic placeholder thumbnail"
+      />
+      <h4>{{ hobby }}</h4>
+      <span class="text-muted">
+        <a href="#" @click="deleteHobby(index)">删除</a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //声明当前子组件接受父组件的属性
+  props: ["hobbies"],
+  methods: {
+    //删除喜好
+    deleteHobby(index) {
+      //触发父组件中 delete_hobby 事件进行删除操作
+      this.$emit("delete_hobby", index);
+    },
+  },
+  template,
+};
+</script>
+
+<style scoped lang="scss"></style>
