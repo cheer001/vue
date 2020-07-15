f9caa87d29edd8407eb2a2676d431ba9c955cb9b
gerry <2312306565@qq.com>
2020-07-15 22:36:15 +0700

    feat(*):解决跨域和开发环境跨域问题

--- VueProject/gerry-msm/src/api/test.js
+++ VueProject/gerry-msm/src/api/test.js
@@ -1,5 +1,6 @@
 import request from "@/utils/request";
 
+const BASE_URL= "/dev-api";
 // request.get("/db.json").then(response => {
 //   console.log(response.data);
 // });
@@ -7,7 +8,7 @@ import request from "@/utils/request";
 //测试2, 以对象配置的方式进行指定请求方式
 request({
   method: "get",
-  url: "/db.json"
+  url: BASE_URL+"/db.json"
 }).then(response => {
   console.log("get2", response.data);
 });
@@ -16,7 +17,7 @@ export default {
   getList() {
     const req = request({
       method: "get",
-      url: "/db.json"
+      url: BASE_URL+"/db.json"
     });
     console.log(req); //Promise.then()
 
