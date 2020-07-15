3925b6733ff87f793b3b1e2694ade87e355a56f7
gerry <2312306565@qq.com>
2020-07-15 22:12:27 +0700

    refactor(*):初始化axios-demo示例

---
+++ VueProject/axios-demo/src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+//注意引入在Vue打的下面
+// ElementUI组件库
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+import App from "./App.vue";
+import router from "./router";
+
+//使用ElementUI
+Vue.use(ElementUI);
+
+Vue.config.productionTip = process.env.NODE_ENV === "production";
+console.log(process.env.NODE_ENV); //开发环境development  ,生产环境production
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
