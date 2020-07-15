b717cb645714b1ff425596b9408ff0271b808e45
gerry <2312306565@qq.com>
2020-07-15 20:30:25 +0700

    chore(*):整合Element UI

--- VueProject/gerry-msm/src/main.js
+++ VueProject/gerry-msm/src/main.js
@@ -1,8 +1,17 @@
 import Vue from "vue";
+//注意引入在Vue打的下面
+// ElementUI组件库
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
 import App from "./App.vue";
 import router from "./router";
 
-Vue.config.productionTip = false;
+//使用ElementUI
+Vue.use(ElementUI);
+
+Vue.config.productionTip = process.env.NODE_ENV === "production";
+console.log(process.env.NODE_ENV); //开发环境development  ,生产环境production
 
 new Vue({
   router,
