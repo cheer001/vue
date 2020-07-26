f7b4f9a5da7af31f83e90315a1c5b75461631c25
paul chris <2312306565@qq.com>
2020-07-26 20:49:13 +0700

    feat(utils/request):添加全局加载状态

--- VueProject/gerry-msm/src/utils/request.js
+++ VueProject/gerry-msm/src/utils/request.js
@@ -1,5 +1,29 @@
 import axios from "axios";
+import { Loading } from "element-ui";
 
+const loading = {
+  loadindInstance: null, //loading实例
+  //打开加载
+  open: function() {
+    //创建实例,而且会打开加载窗口
+    //单例模式 实例为空则创建
+    if (this.loadindInstance === null) {
+      this.loadindInstance = Loading.service({
+        target: ".main",
+        text: "拼了老命加载钟...",
+        background: "rgba(0, 0, 0, 0.5)"
+      });
+    }
+  },
+  //关闭加载
+  close: function() {
+    if (this.loadindInstance != null) {
+      this.loadindInstance.close();
+    }
+    //关闭加载后实例至为空
+    this.loadindInstance = null;
+  }
+};
 // axios.get("/db.json").then(response => {
 //   const data = response.data;
 //   console.log(data);
@@ -23,9 +47,13 @@ const request = axios.create({
 request.interceptors.request.use(
   config => {
     //请求的拦截
+
+    //打开加载窗口
+    loading.open();
     return config;
   },
   error => {
+    loading.close();
     //出现异常的时候
     return Promise.reject(error);
   }
@@ -33,10 +61,12 @@ request.interceptors.request.use(
 //响应拦截器
 request.interceptors.response.use(
   response => {
-    //   response.data
+    //关闭加载窗口
+    loading.close();
     return response;
   },
   error => {
+    loading.close();
     return Promise.reject(error);
   }
 );
